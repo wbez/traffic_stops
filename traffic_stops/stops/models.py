e127d6a619ea4b84ff5016f66bf14e23184d2fd5
mattkiefer <matthewlkiefer@gmail.com>
2022-08-15 15:26:09 -0500

    updated models but they are v broken rn

--- traffic_stops/stops/models.py
+++ traffic_stops/stops/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
 # Create your models here.
+class Stop(models.model):
+    AgencyCode = models.TextField()
+    AgencyName = models.CharField(max_length=100,null=True)
+    DateOfStopTimeOfStop
+    DurationOfStop
+    ZIP
+    VehicleMake
+    VehicleYear
+    DriversYearofBirth
+    DriverSex
+    DriverRace
+    ReasonForStop
+    TypeOfMovingViolation
+    ResultOfStop
+    BeatLocationOfStop
+    VehicleConsentSearchRequested
+    VehicleConsentGiven
+    VehicleSearchConducted
+    VehicleSearchConductedBy
+    VehicleContrabandFound
+    VehicleDrugsFound
+    VehicleDrugParaphernaliaFound
+    VehicleAlcoholFound
+    VehicleWeaponFound
+    VehicleStolenPropertyFound
+    VehicleOtherContrabandFound
+    VehicleDrugAmount
+    DriverConsentSearchRequested
+    DriverConsentGiven
+    DriverSearchConducted
+    DriverSearchConductedBy
+    PassengerConsentSearchRequested
+    PassengerConsentGiven
+    PassengerSearchConducted
+    PassengerSearchConductedBy
+    DriverPassengerContrabandFound
+    DriverPassengerDrugsFound
+    DriverPassengerDrugParaphernaliaFound
+    DriverPassengerAlcoholFound
+    DriverPassengerWeaponFound
+    DriverPassengerStolenPropertyFound
+    DriverPassengerOtherContrabandFound
+    DriverPassengerDrugAmount
+    PoliceDogPerformSniffOfVehicle
+    PoliceDogAlertIfSniffed
+    PoliceDogVehicleSearched
+    PoliceDogContrabandFound
+    PoliceDogDrugsFound
+    PoliceDogDrugParaphernaliaFound
+    PoliceDogAlcoholFound
+    PoliceDogWeaponFound
+    PoliceDogStolenPropertyFound
+    PoliceDogOtherContrabandFound
+    PoliceDogDrugAmount
